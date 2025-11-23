584657a308e665117f95d8a164cc6c7fd39d6534
Wompenator <milktec.2.0@gmail.com>
2025-11-23 23:07:24 +1300

    added lazy.nvim setup

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,11 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.laststatus = 3
+vim.opt.showmode = false
+vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.virtualedit = "block"
+vim.opt.signcolumn = "yes"
+vim.opt.pumblend = 10
+
